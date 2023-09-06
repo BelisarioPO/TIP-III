695dfeb2f1ec21254b124b04b8bc7d76ee60d299
Tomas Ruiz <truiz@consultatioinvestments.com>
2023-09-06 09:30:20 -0300

    todo roto

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,42 +19,39 @@ class Home extends Component {
     }
      //CIERRA FORMS REACT
 
-    componentDidMount() {
+     componentDidMount() {
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=b76faeee5fc3002a166c7f5c929c2c33&language=en-US&page=1')
             .then(response => response.json())
-
-            .then(data => this.setState(
-                this.state.datos = data.results
-            ))
-
-            .catch (error => console.log(error) );
+            .then(data => this.setState({ datos: data.results }))
+            .catch(error => console.log(error));
     }
 
-
     render() {
-        return (
-            <section className=''>
-            <form onSubmit={(event)=>this.evitarSubmit(event)}>
+    return (
+        <section className=''>
+            <form onSubmit={(event) => this.evitarSubmit(event)} className=''>
                 <label> FORMS</label>
-                <input type='text' onChange={(event)=>this.controlarCambios(event)} value={this.state.forms}/>
-                <input type='submit' value = "Submit"/>
-            
+                <input type='text' onChange={(event) => this.controlarCambios(event)} value={this.state.forms} className='buscador' />
+                <input type='submit' value="Submit" className='' />
+
             </form>
 
+            {this.state.datos ? (
+                this.state.datos.map((Obj, i) => {
+                    console.log(this.state);
+                    if (i < 5) {
+                        return (<Pelicula title={ Obj.title} poster={Obj.poster_path} description={Obj.overview} id={Obj.id} />)
+                    }
+                    else { return (null) }
+                })
+            ) : (
+                <p>Loading...</p> // Muestra un mensaje de carga mientras se obtienen los datos.
+            )}
 
-                {
-                    this.state.datos.map((Obj, i)=>{
-                        console.log(this.state);
-                        if (i<5) {
-                            return(<Pelicula title={ this.props.espeli ? Obj.title : Obj.name} poster={Obj.poster_path} description={Obj.overview} id={Obj.id}/>)
-                        }
-                        else{return (null)}
-                    })
-                }
+        </section>
 
-            </section>
-            
-        )}
+    )
+}
 }
 
 export default Home;
--- src/components/Pelicula/Pelicula.js
+++ src/components/Pelicula/Pelicula.js
@@ -25,11 +25,11 @@ class Pelicula extends Component {
 
     render() {
         return(<article className=''>
-        <img src={`https://image.tmbd.org/t/p/w500/${this.props.poster}`} alt={this.props.title} className='' />
-        <a href="" className="">Ver Mas</a>
+        <img src={`https://image.tmbd.org/t/p/w500/${this.props.poster}`} alt={this.props.title} className='img-index' />
+        
         <button onClick={() => this.mostrarDetalles()} className="" type="button">{this.state.BotonDesc}</button>
         <button onClick={() => this.agregarFav(this.props.id)} className="" type="button">{this.state.BotonFavorito}</button>
-        <p className="">{this.props.title}</p>
+        <p className="Pelis">{this.props.title}</p>
         <p class={this.state.ocultarInfo ? 'Ocultar' : 'Ver Mas'}>{this.props.description}</p>
     </article>)
     }

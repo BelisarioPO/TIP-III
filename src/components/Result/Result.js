7e93e3200dec04c2a322bd06682ebf8440f60169
TaylorTomas <ttaylor@udesa.edu.ar>
2023-09-15 16:19:51 -0300

    no funciona el buscador por tema de duplicados

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,103 +1,82 @@
 import React, { Component } from 'react';
 import Pelicula from '../Pelicula/Pelicula';
+
 class Result extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        peliculasPopulares: [],
-        peliculasProximas: [],
-        error: null,
-        query: '', // Para almacenar el título de búsqueda
-      };
-    }
-  
-    componentDidMount() {
-      // Realiza la solicitud de películas populares
-      fetch(
-        'https://api.themoviedb.org/3/movie/popular?api_key=b76faeee5fc3002a166c7f5c929c2c33&language=en-US&page=1'
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.results) {
-            this.setState({ peliculasPopulares: data.results });
-          } else {
-            this.setState({ error: 'Error al obtener películas populares' });
-          }
-        })
-        .catch((error) => {
-          this.setState({ error: 'Error al obtener películas populares' });
-        });
-  
-      // Realiza la solicitud de próximas películas
-      fetch(
-        'https://api.themoviedb.org/3/movie/upcoming?api_key=b76faeee5fc3002a166c7f5c929c2c33&language=en-US&page=1'
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.results) {
-            this.setState({ peliculasProximas: data.results });
-          } else {
-            this.setState({ error: 'Error al obtener próximas películas' });
-          }
-        })
-        .catch((error) => {
-          this.setState({ error: 'Error al obtener próximas películas' });
-        });
-    }
-  
-    // Función para manejar el cambio en el campo de búsqueda
-    handleSearchChange = (event) => {
-      this.setState({ query: event.target.value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      resultadosPopulares: [],
+      resultadosProximas: [],
+      error: null,
     };
-  
-    // Función para filtrar las películas basadas en la búsqueda
-    filtrarPorTitulo = (movies) => {
-      const { query } = this.state;
-      return movies.filter((pelicula) =>
-        pelicula.title.toLowerCase().includes(query.toLowerCase())
-      );
-    };
-  
-    render() {
-      const { peliculasPopulares, peliculasProximas, error, query } = this.state;
-  
-      const peliculasPopularesFiltradas = this.filtrarPorTitulo(
-        peliculasPopulares
-      );
-      const peliculasProximasFiltradas = this.filtrarPorTitulo(
-        peliculasProximas
-      );
-  
-      return (
-        <div className='resultado-busqueda'>
-          <input
-            className='buscador'
-            type='text'
-            placeholder='Buscar películas'
-            value={query}
-            onChange={this.handleSearchChange}
-          />
-          {error ? (
-            <p>{error}</p>
-          ) : (
-            <div>
-              <h2>Películas Populares</h2>
-              <ul>
-                {peliculasPopularesFiltradas.map((Obj) => (
-                  <Pelicula title={Obj.title} poster={Obj.poster_path} description={Obj.overview} id={Obj.id} />
-                ))}
-              </ul>
-  
-              <h2>Próximas Películas</h2>
-              <ul>
-                {peliculasProximasFiltradas.map((Obj) => (
-                  <Pelicula title={Obj.title} poster={Obj.poster_path} description={Obj.overview} id={Obj.id} />
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      );
-    }
   }
-    export default Result;
+
+  componentDidMount() {
+    const { query } = this.props.match.params;
+
+    // Realiza la solicitud de películas populares
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=b76faeee5fc3002a166c7f5c929c2c33&language=en-US&query=${query}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results) {
+          this.setState({ resultadosPopulares: data.results });
+        } else {
+          this.setState({ error: 'Error al obtener películas populares' });
+        }
+      })
+      .catch((error) => {
+        this.setState({ error: 'Error al obtener películas populares' });
+      });
+
+    // Realiza la solicitud de próximas películas
+    fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=b76faeee5fc3002a166c7f5c929c2c33&language=en-US&query=${query}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results) {
+          this.setState({ resultadosProximas: data.results });
+        } else {
+          this.setState({ error: 'Error al obtener próximas películas' });
+        }
+      })
+      .catch((error) => {
+        this.setState({ error: 'Error al obtener próximas películas' });
+      });
+  }
+
+  render() {
+    const { resultadosPopulares, resultadosProximas, error } = this.state;
+
+    // Combina los resultados de ambas solicitudes
+    const resultadosTotales = [...resultadosPopulares, ...resultadosProximas];
+
+    
+
+    return (
+      <div className='resultado-busqueda'>
+        {error ? (
+          <p>{error}</p>
+        ) : (
+          <div>
+            <h2>Resultados de la búsqueda</h2>
+            <ul>
+              {resultadosTotales.map((pelicula) => (
+                <Pelicula
+                  title={pelicula.title}
+                  poster={pelicula.poster_path}
+                  description={pelicula.overview}
+                  id={pelicula.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Result;
